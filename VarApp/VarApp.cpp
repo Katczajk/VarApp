04be1e5234eedf3441a02f7dce91ce875c48d11c
Katczajk <43179276+Katczajk@users.noreply.github.com>
2020-04-02 16:31:52 +0200

    update 02-04-2020 / 16:31

--- VarApp/VarApp.cpp
+++ VarApp/VarApp.cpp
@@ -2,6 +2,8 @@
 //
 
 #include <iostream>
+#include <string>
+#include <windows.h>
 
 #ifndef VARAPP_H
 #define VARAPP_H
@@ -11,6 +13,26 @@
 #endif // !VARAPP_H
 
 const int INTMAX = INT_MAX;
+
+
+void makeSome() {
+	std::string myVarStr;
+	int Num100 = 100;
+	myVarStr = static_cast<int>(Num100);
+
+	Suit st2 = Suit::Diamonds;
+	char s2 = static_cast<char>(st2);
+	// MessageBox(nullptr, L"Text", L"Caption", MB_OK);
+	box l_info;
+	l_info.text = L"My Text";
+	l_info.caption = L"My Caption";
+	show_box(l_info);
+}
+
+void show_box(box Box)
+{
+	MessageBox(nullptr, Box.text, Box.caption, MB_OK);
+}
 
 int main()
 {
@@ -26,6 +48,8 @@ int main()
 
 	// std::cout << *Number << std::endl;
 	get_int(&j,&c);
+
+	makeSome();
 
 	getchar();
 }
@@ -44,7 +68,14 @@ void get_int(int * INT, char * CHR)
 	std::cout << *myTarget << std::endl;
 
 	Suit Card = Suit::Diamonds;
-	PlayCard(Card);
+	/* we cast here Suit::Diamonds into int Num10 Var */
+	int Num10 = static_cast<int>(Suit::Diamonds);
+	PlayCard(Card, Num10);
+
+	login f_User;
+	f_User.username = "Testuser";
+	f_User.password = "TopSecret";
+	try_login(f_User);
 
 }
 
@@ -54,9 +85,18 @@ double get_pi(const double pi)
 	return pi;
 }
 
-void PlayCard(Suit suit)
+void try_login(login Login)
+{
+	// printf(Login.username);
+	// printf(Login.password);
+	std::string st_r = "GoGo Girls";
+	std::cout << st_r << std::endl;
+}
+
+void PlayCard(Suit suit, int Zahl1)
 {
 	if (suit == Suit::Diamonds) {
 		std::cout << "Suit has entered a Function and got working with Value " << Suit::Diamonds << std::endl;
+		std::cout << "int Var Zahl1 has also reached Fuction with Value " << Zahl1 << " it works" << std::endl;
 	}
 }
--- VarApp/VarApp.h
+++ VarApp/VarApp.h
@@ -8,5 +8,21 @@ void get_int(int* INT, char * CHR);
 /* do not in main function setup to 3.14 only at init here */
 double get_pi(const double pi = 3.14);
 
+/* first declared enumerator */
 enum Suit { Diamonds = 5 };
-void PlayCard(Suit suit);
+
+/* here a struct object */
+struct login {
+	std::string username = "";
+	std::string password = "";
+};
+
+struct box {
+	LPCWSTR caption;
+	LPCWSTR text;
+};
+
+void try_login(login Login);
+void PlayCard(Suit suit, int Zahl1);
+void makeSome();
+void show_box(box Box);
